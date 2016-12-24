0f5b626226d3b0a8abf7556370978e21d8704180
Mykhaylo <basenko.mykhaylo@hotmail.com>
2016-12-24 13:50:22 +0200

    move js to file'

---
+++ js/script.js
@@ -0,0 +1,7 @@
+﻿  $(document).ready(function() {	
+        $('.getting-list-selected-box').multiselect({
+                buttonWidth: '100%'
+				, maxHeight: 200
+				,nSelectedText: '-Три і більше'
+        });
+    });
